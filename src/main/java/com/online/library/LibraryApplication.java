6b5e660e09bec4d4a7687c43ae8d9a63bb03db33
Tomek Petrykowski <tomasz.petrykowski.20@gmail.com>
2026-01-16 14:11:26 +0100

    Test connection to PostgreSQL db

--- src/main/java/com/online/library/LibraryApplication.java
+++ src/main/java/com/online/library/LibraryApplication.java
@@ -1,13 +1,30 @@
 package com.online.library;
 
+import lombok.extern.java.Log;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+import javax.sql.DataSource;
+@Log
 @SpringBootApplication
-public class LibraryApplication {
+public class LibraryApplication implements CommandLineRunner {
+
+    private final DataSource dataSource;
+
+    public LibraryApplication(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApplication.class, args);
 	}
 
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("Connected to database: " + dataSource.getConnection());
+        final JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        jdbcTemplate.execute("SELECT 1");
+    }
 }
