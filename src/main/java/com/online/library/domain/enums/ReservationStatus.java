78bb819e605d2cb4dcef38ea5f8100cedd4a5474
Tomek Petrykowski <tomasz.petrykowski.20@gmail.com>
2026-01-17 22:22:25 +0100

    Add enums that match db schema

---
+++ src/main/java/com/online/library/domain/enums/ReservationStatus.java
@@ -0,0 +1,5 @@
+package com.online.library.domain.enums;
+
+public enum ReservationStatus {
+    OCZEKUJĄCA, POTWIERDZONA, WYPOŻYCZONA, ZWRÓCONA
+}
---
+++ src/main/java/com/online/library/domain/enums/UserRole.java
@@ -0,0 +1,5 @@
+package com.online.library.domain.enums;
+
+public enum UserRole {
+    ADMIN, USER
+}
