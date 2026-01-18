4f7c34b841f55d53d630a86f4645e31d4839d84f
Tomek Petrykowski <tomasz.petrykowski.20@gmail.com>
2026-01-18 16:03:40 +0100

    Refactor CommentRepository to have usefull methods

--- src/main/java/com/online/library/repositories/CommentRepository.java
+++ src/main/java/com/online/library/repositories/CommentRepository.java
@@ -1,9 +1,21 @@
 package com.online.library.repositories;
 
 import com.online.library.domain.entities.CommentEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
+
+    List<CommentEntity> findByBookIdOrderByCreatedAtDesc(Long bookId);
+
+    Page<CommentEntity> findByBookIdOrderByCreatedAtDesc(Long bookId, Pageable pageable);
+
+    List<CommentEntity> findByUserId(Long userId);
+
+    Long countByBookId(Long bookId);
 }
