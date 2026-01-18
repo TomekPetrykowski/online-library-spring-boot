8afabe01382576c5769ecc9480caf4b5197800f2
Tomek Petrykowski <tomasz.petrykowski.20@gmail.com>
2026-01-18 16:54:13 +0100

    Clear up app entrypoint

--- src/main/java/com/online/library/LibraryApplication.java
+++ src/main/java/com/online/library/LibraryApplication.java
@@ -1,30 +1,15 @@
 package com.online.library;
 
 import lombok.extern.java.Log;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.JdbcTemplate;
 
-import javax.sql.DataSource;
 @Log
 @SpringBootApplication
-public class LibraryApplication implements CommandLineRunner {
+public class LibraryApplication {
 
-    private final DataSource dataSource;
-
-    public LibraryApplication(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryApplication.class, args);
     }
 
-	public static void main(String[] args) {
-		SpringApplication.run(LibraryApplication.class, args);
-	}
-
-    @Override
-    public void run(String... args) throws Exception {
-        log.info("Connected to database: " + dataSource.getConnection());
-        final JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-        jdbcTemplate.execute("SELECT 1");
-    }
 }
